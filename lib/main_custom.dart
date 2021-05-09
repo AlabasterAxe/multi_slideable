f9b651cf320b6fcbddb773814de161115761af76
Matthew Keller <matthew.ed.keller@gmail.com>
2021-05-09 12:13:34 -0400

    multi select working

--- lib/main_custom.dart
+++ lib/main_custom.dart
@@ -1,57 +1,194 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
-  _MyAppState createState() => _MyAppState();
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Mail Client',
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text('Inbox'),
+          ),
+          body: MyHomePage(),
+        ));
+  }
 }
 
-class _MyAppState extends State<MyApp> {
-  double dragAmount = 0;
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
+  AnimationController? dragAnimation;
   double containerHeight = 50;
   int? startIndex;
   int? offset;
+  ScrollController sc = ScrollController();
+  double actionThreshold = .2;
+  List<int> emails = List.generate(20, (i) => i);
 
-  List<Widget> getListTiles() {
+  @override
+  void initState() {
+    super.initState();
+    dragAnimation = AnimationController.unbounded(value: 0, vsync: this);
+  }
+
+  Widget getActionRect() {
+    if (!sc.hasClients || startIndex == null || offset == null) {
+      return Container();
+    }
+
+    double screenWidth = MediaQuery.of(context).size.width;
+    double actionThresholdPixels = screenWidth * actionThreshold;
+
+    int topIndex = min(startIndex!, startIndex! + offset!);
+    int bottomIndex = max(startIndex!, startIndex! + offset!);
+
+    double topPixel = topIndex * 50 - sc.offset;
+    double bottomPixel = (bottomIndex + 1) * 50 - sc.offset;
+
+    if (dragAnimation!.value > 0) {
+      return AnimatedBuilder(
+          animation: dragAnimation!,
+          builder: (context, snapshot) {
+            return Positioned(
+                left: 0,
+                width: dragAnimation!.value,
+                top: topPixel,
+                height: bottomPixel - topPixel,
+                child: AnimatedContainer(
+                    color: dragAnimation!.value.abs() > actionThresholdPixels
+                        ? Colors.green
+                        : Colors.grey,
+                    duration: Duration(milliseconds: 200),
+                    child: Center(
+                        child: Icon(Icons.archive, color: Colors.white))));
+          });
+    } else {
+      return AnimatedBuilder(
+          animation: dragAnimation!,
+          builder: (context, snapshot) {
+            return Positioned(
+                right: 0,
+                width: dragAnimation!.value.abs(),
+                top: topPixel,
+                height: bottomPixel - topPixel,
+                child: AnimatedContainer(
+                    color: dragAnimation!.value.abs() > actionThresholdPixels
+                        ? Colors.red
+                        : Colors.grey,
+                    duration: Duration(milliseconds: 200),
+                    child: Center(
+                        child: Icon(Icons.delete, color: Colors.white))));
+          });
+    }
+  }
+
+  void removeItemsOnAnimationCompletion() {
+    void Function(AnimationStatus)? removeItemListener;
+    removeItemListener = (status) {
+      if (status == AnimationStatus.completed) {
+        int topIndex = min(startIndex!, startIndex! + offset!);
+        int bottomIndex = max(startIndex!, startIndex! + offset!);
+        emails.removeRange(topIndex, bottomIndex + 1);
+        startIndex = null;
+        offset = null;
+        dragAnimation!.removeStatusListener(removeItemListener!);
+        dragAnimation!.value = 0;
+        setState(() {});
+      }
+    };
+    dragAnimation!.addStatusListener(removeItemListener);
+  }
+
+  void animateDragEnd() {
+    double screenWidth = MediaQuery.of(context).size.width;
+    if (dragAnimation!.value > screenWidth * actionThreshold) {
+      dragAnimation!.animateTo(screenWidth,
+          curve: Curves.easeInOut, duration: Duration(milliseconds: 300));
+      removeItemsOnAnimationCompletion();
+    } else if (dragAnimation!.value < -(screenWidth * actionThreshold)) {
+      dragAnimation!.animateTo(-screenWidth,
+          curve: Curves.easeInOut, duration: Duration(milliseconds: 300));
+      removeItemsOnAnimationCompletion();
+    } else {
+      dragAnimation!.animateBack(0,
+          curve: Curves.easeInOut, duration: Duration(milliseconds: 300));
+    }
+  }
+
+  List<Widget> getListTiles(items) {
     List<Widget> results = [];
-    for (int i = 0; i < 10; i++) {
+    for (int i = 0; i < items.length; i++) {
       results.add(
-        Transform.translate(
-          offset: startIndex != null &&
-                  offset != null &&
-                  (i >= startIndex! && i <= startIndex! + offset! ||
-                      i >= startIndex! + offset! && i <= startIndex!)
-              ? new Offset(dragAmount, 0)
-              : new Offset(0, 0),
-          child: GestureDetector(
-            onHorizontalDragStart: (DragStartDetails deets) {
-              startIndex = i;
-              offset = 0;
-            },
-            onHorizontalDragUpdate: (DragUpdateDetails deets) {
-              if (deets.primaryDelta != null) {
-                setState(() {
-                  dragAmount += deets.primaryDelta!;
-                });
-              }
-              offset = (deets.localPosition.dy / containerHeight).floor();
-            },
-            onHorizontalDragEnd: (DragEndDetails deets) {
+        GestureDetector(
+          key: Key('${items[i]}'),
+          onHorizontalDragStart: (DragStartDetails deets) {
+            startIndex = i;
+            offset = 0;
+          },
+          onHorizontalDragUpdate: (DragUpdateDetails deets) {
+            if (deets.primaryDelta != null) {
               setState(() {
-                dragAmount = 0;
-                startIndex = null;
-                offset = null;
+                dragAnimation!.value += deets.primaryDelta!;
               });
-            },
-            child: Container(
-              height: containerHeight,
-              color: Colors.amber[600],
-              child: Center(child: Text('Entry $i')),
-            ),
-          ),
+            }
+            offset = (deets.localPosition.dy / containerHeight).floor();
+          },
+          onHorizontalDragEnd: (DragEndDetails deets) {
+            setState(() {
+              animateDragEnd();
+            });
+          },
+          child: AnimatedBuilder(
+              animation: dragAnimation!,
+              builder: (context, child) {
+                return Transform.translate(
+                  offset: startIndex != null &&
+                          offset != null &&
+                          (i >= startIndex! && i <= startIndex! + offset! ||
+                              i >= startIndex! + offset! && i <= startIndex!)
+                      ? new Offset(dragAnimation!.value, 0)
+                      : new Offset(0, 0),
+                  child: Container(
+                    height: containerHeight - 1,
+                    color: Colors.white,
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text('Buy more spam! ${items[i]}',
+                                style: TextStyle(fontWeight: FontWeight.bold),
+                                textAlign: TextAlign.start),
+                            Text(
+                              'Now with twice the salt!',
+                              textAlign: TextAlign.start,
+                            ),
+                          ]),
+                    ),
+                  ),
+                );
+              }),
         ),
       );
+      results.add(AnimatedBuilder(
+          animation: dragAnimation!,
+          builder: (context, snapshot) {
+            return Divider(
+                height: 1,
+                thickness: 1,
+                indent: dragAnimation!.value > 0 ? dragAnimation!.value : 0,
+                endIndent:
+                    dragAnimation!.value < 0 ? dragAnimation!.value.abs() : 0);
+          }));
     }
 
     return results;
@@ -59,13 +196,9 @@ class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Material App',
-      home: Scaffold(
-          appBar: AppBar(
-            title: Text('Material App Bar'),
-          ),
-          body: ListView(children: getListTiles())),
-    );
+    return Stack(children: [
+      getActionRect(),
+      ListView(children: getListTiles(emails), controller: sc)
+    ]);
   }
 }
